857447ea8431d6607733cfaa2dfb147e3c39cfd9
Kaique Gama <kaiquesilva49@hotmail.com>
2023-05-20 22:07:57 -0300

    layout

--- app/src/main/java/alura/orgs/MainActivity.kt
+++ app/src/main/java/alura/orgs/MainActivity.kt
@@ -2,11 +2,12 @@ package alura.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.TextView
+
 
 class MainActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this,"hello kotlin",Toast.LENGTH_LONG).show()
+        setContentView(R.layout.activity_main)
     }
 }
