104acf8b9163a9876faa08adc803930b6cf4c6b3
Kaique Gama <kaiquesilva49@hotmail.com>
2023-05-21 19:42:25 -0300

    recyclerview

--- app/src/main/java/alura/orgs/MainActivity.kt
+++ app/src/main/java/alura/orgs/MainActivity.kt
@@ -7,7 +7,14 @@ import android.widget.TextView
 
 class MainActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val nome = findViewById<TextView>(R.id.nome)
+//        val descricao = findViewById<TextView>(R.id.descricao)
+//        val preco = findViewById<TextView>(R.id.preco)
+//
+//        nome.text = "Teste"
+//        descricao.text = "Teste"
+//        preco.text = "Teste"
     }
 }
