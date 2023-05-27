8dddd4a880f870e934e17fb984e315a0771680d9
Kaique Gama <kaiquesilva49@hotmail.com>
2023-05-27 13:54:16 -0300

    implementando formulario

---
+++ app/src/main/java/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,8 @@
+package alura.orgs.ui.activity
+
+import alura.orgs.R
+import androidx.appcompat.app.AppCompatActivity
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+
+}
--- app/src/main/java/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/alura/orgs/ui/activity/MainActivity.kt
@@ -5,15 +5,16 @@ import alura.orgs.model.Produto
 import alura.orgs.ui.recylerview.adapter.ListaProdutoAdapter
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.math.BigDecimal
 
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView);
 
         recyclerView.adapter = ListaProdutoAdapter(
